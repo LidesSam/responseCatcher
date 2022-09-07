407a9f26056fabd7eca2b008b6a1c55e86227c48
LidesSam <lides.sam@gmail.com>
2022-09-07 10:40:10 -0300

    first commit

---
+++ simplifiedDemo.py
@@ -0,0 +1,11 @@
+import tensorflow as tf
+import numpy as np
+
+reesponse=np.array(["excelente","24","buen servicio","rapido","no","5","5 estrellas"],dtype=float)
+results=np.array(["valid","invalid","valid","valid","invalid","valid"],dtype=String)
+
+entrylayer=
+model =tf.keras.layer.Dense(units=1,input_shape=[1])
+model.copile(){
+    
+}
